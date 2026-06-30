67a92ad80dd3659e78f88bbd549c4497bfd9317d
t3l9 <150610751+t3l9@users.noreply.github.com>
2026-06-30 12:33:00 +0300

    улучшение CORS

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://analytics-uvao.ru', 'https://www.analytics-uvao.ru'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
